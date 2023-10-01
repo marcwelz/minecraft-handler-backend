0fc6f378fa9bd858e8ec46817a6be613f52eb5d4
marcwelz <marc.welz03@gmail.com>
2023-10-01 15:39:02 +0200

    update server.ts code for better readibilty

--- server.ts
+++ server.ts
@@ -3,7 +3,21 @@ import { oakCors } from "https://deno.land/x/cors/mod.ts";
 const app = new Application();
 const port = 8000;
 
+/*
+  ----- SERVER SETTINGS -----
+  10: Defines the mode of gameplay.
+  19: Enable/disable PvP on the server.
+  26: The maximum number of players that can play on the server.
+  36: The "level-name" value is used as the world name and its folder name.
+  49: Enable remote access to the server console.
+
+  SERVER_IP : Set server IP (ex. 127.0.0.1:25565)
+  SERVER_RAM: Set server ram in MB (1 GB = 1024 MB)
+ */
+
 const availableSettings = [10, 19, 36, 26, 49];
+const SERVER_IP = "192.168.1.232:25565";
+const SERVER_RAM = "2048";
 
 app.use(
   oakCors({
@@ -13,37 +27,37 @@ app.use(
 
 const router = new Router();
 const encoder = new TextEncoder();
-const decoder = new TextDecoder();
 
-let p: any;
+let gameConsole: any;
 
-var serverStatus = { status: false };
+const serverStatus = {status: false};
 
 router
   .get("/server/status", (ctx) => {
-    ctx.response.body = { serverStatus, ip: "192.168.1.232:25565" };
+    ctx.response.body = { serverStatus, ip: SERVER_IP };
   })
   .get("/server/start", (ctx) => {
-    initialServer();
-    ctx.response.body = serverStatus;
+    initialServer().then(() => ctx.response.body = serverStatus);
   })
   .get("/server/stop", (ctx) => {
-    initialServer();
-    ctx.response.body = serverStatus;
+    stopServer().then(() => ctx.response.body = serverStatus);
   })
   .post("/server/command", async (ctx) => {
-    if (p !== null) {
+    if (gameConsole) {
       const values = await ctx.request.body().value;
-      if (values.command.charAt(0) !== "/") {
+
+      // Check if command
+      if (!values.command.startsWith("/")) {
         values.command = "/" + values.command;
       }
-      console.log(values.command);
-      await p.stdin.write(encoder.encode(values.command + "\n"));
+
+      // Write to console
+      await gameConsole.stdin.write(encoder.encode(values.command + "\n"));
     }
   })
   .post("/server/settings", async (ctx) => {
     const postvalue = await ctx.request.body().value;
-    settings(postvalue.settings);
+    await settings(postvalue.settings);
   })
   .get("/server/settings/get", async (ctx) => {
     ctx.response.body = await getSettings();
@@ -52,17 +66,12 @@ router
 async function settings(value: any) {
   const text = await Deno.readTextFile("server.properties");
   let textSplit = text.split("\n");
-  for (let i = 0; i < availableSettings.length; i++) {
-    let row = textSplit[availableSettings[i]].split("=");
-
-    row[1] = value[i];
-    textSplit.splice(availableSettings[i], 1, row[0] + "=" + row[1]);
+  for (const settingIndex of availableSettings) {
+    const [key, _] = textSplit[settingIndex].split("=");
+    textSplit[settingIndex] = `${key}=${value[settingIndex]}`;
   }
 
-  let writetext = "";
-  for (let i = 0; i + 1 < textSplit.length; i++) {
-    writetext = writetext + textSplit[i] + "\n";
-  }
+  const writetext = textSplit.slice(0, -1).join('\n');
 
   await Deno.remove("server.properties");
   await Deno.create("server.properties");
@@ -72,47 +81,43 @@ async function settings(value: any) {
 }
 
 async function getSettings() {
-  const text = await Deno.readTextFile("server.properties");
+  const serverProperties = await Deno.readTextFile("server.properties");
 
-  let textSplit = text.split("\n");
+  const textSplit = serverProperties.split("\n");
 
-  let row = [];
-  for (let i = 0; i < availableSettings.length; i++) {
-    row[i] = textSplit[availableSettings[i]].split("=");
+  const settings = availableSettings.reduce((acc, settingIndex, index) => {
+    const [_, value] = textSplit[settingIndex].split("=");
+    const settingName = Object.keys(acc)[index];
+    return { ...acc, [settingName]: value };
+  }, {});
+
+  return settings;
+}
+
+async function stopServer(){
+  if (gameConsole) {
+    serverStatus.status = false;
+    try {
+      await gameConsole.stdin.write(encoder.encode("/stop"));
+    } catch {
+      console.log("closing the server failed");
+    }
+    await gameConsole.stdin.close();
+    console.log("Server closed.")
   }
-
-  return {
-    gamemode: row[0][1],
-    pvp: row[1][1],
-    worldname: row[2][1],
-    maxplayers: row[3][1],
-    motd: row[4][1],
-  };
 }
 
 async function initialServer() {
   if (!serverStatus.status) {
     serverStatus.status = true;
-    p = Deno.run({
-      cmd: ["java", "-Xmx1024M", "-Xms1024M", "-jar", "server.jar", "nogui"],
+    gameConsole = Deno.run({
+      cmd: ["java", "-Xmx" + SERVER_RAM + "M", "-Xms" + SERVER_RAM + "M", "-jar", "server.jar", "nogui"],
       stdout: "piped",
       stdin: "piped",
       stderr: "piped",
     });
 
-    Deno.copy(p.stdout, Deno.stdout);
-    Deno.copy(p.stdout, Deno.stdout);
-  } else {
-    if (p !== null) {
-      serverStatus.status = false;
-      try {
-        await p.stdin.write(encoder.encode("/stop"));
-      } catch {
-        console.log("closing the server failed");
-      }
-
-      await p.stdin.close();
-    }
+    Deno.copy(gameConsole.stdout, Deno.stdout);
   }
 }
 
